836dfb89ee616dc6a626de84b8a63f5c9dcded7a
Aryan Raj <aryan.raj15604@gmail.com>
2024-01-01 22:37:09 +0530

    Removed transparent background

--- lua/core/plugin_config/gruvbox.lua
+++ lua/core/plugin_config/gruvbox.lua
@@ -3,5 +3,5 @@
 
 
 vim.cmd.colorscheme('gruvbox')
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
