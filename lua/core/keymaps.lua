6fec77e58e2504ebf7e1b559e1a6bcc475b2e192
Aryan Raj <aryan.raj15604@gmail.com>
2024-01-09 00:15:36 +0530

    Added window split commands

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,6 +19,14 @@ vim.opt.shiftround = true
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>s', ':split<CR>')
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>')
+
+-- move topleft window to different positions
+vim.keymap.set('n', '<leader>th', '<C-w>t<C-w>H')
+vim.keymap.set('n', '<leader>tj', '<C-w>t<C-w>J')
+vim.keymap.set('n', '<leader>tk', '<C-w>t<C-w>K')
+vim.keymap.set('n', '<leader>tl', '<C-w>t<C-w>L')
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
