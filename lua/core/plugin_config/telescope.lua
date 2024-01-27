230bf4fd661c6334e0254befbbcb71914b5a082f
Aryan Raj <aryan.raj15604@gmail.com>
2024-01-27 20:19:57 +0530

    Added separate keybunding <Leader>fp to search through git files

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -24,27 +24,9 @@ telescope.setup({
 	},
 })
 
--- setting up git_file, find_files
-local is_inside_work_tree = {}
-
-local project_files = function()
-  local opts = {} -- define here if you want to define something
-
-  local cwd = vim.fn.getcwd()
-  if is_inside_work_tree[cwd] == nil then
-    vim.fn.system("git rev-parse --is-inside-work-tree")
-    is_inside_work_tree[cwd] = vim.v.shell_error == 0
-  end
-
-  if is_inside_work_tree[cwd] then
-    builtin.git_files(opts)
-  else
-    builtin.find_files(opts)
-  end
-end
-
-vim.keymap.set('n', '<Space>ff', project_files, {})
+vim.keymap.set('n', '<Space>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<Space>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<Space>fp', builtin.git_files, {})
