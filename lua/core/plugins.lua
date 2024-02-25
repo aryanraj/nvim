38560478b1b823621ed2887ba52f6c185a4f595b
Aryan Raj <aryan.raj15604@gmail.com>
2024-02-25 11:45:25 +0530

    Added folke/zen-mode.nvim and folke/twilight.nvim

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -85,6 +85,12 @@ return require('packer').startup(function(use)
     requires = { 'nvim-tree/nvim-web-devicons' },
   }
 
+  -- Zenmode
+  use {
+    'folke/zen-mode.nvim',
+    requires = { 'folke/twilight.nvim' },
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
