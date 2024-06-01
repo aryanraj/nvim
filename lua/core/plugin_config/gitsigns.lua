af40dc4ea72317f232611881b82f7e38dd1b0c8c
Aryan Raj <aryan.raj15604@gmail.com>
2024-06-01 14:50:08 +0530

    Gitsigns current line blame and hunk preview

--- lua/core/plugin_config/gitsigns.lua
+++ lua/core/plugin_config/gitsigns.lua
@@ -5,5 +5,17 @@ require('gitsigns').setup({
     delete = { text = '_' },
     topdelete = { text = '‾' },
     changedelete = { text = '~' },
-  }
+  },
+  current_line_blame = true,
+  on_attach = function (bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+  end
 })
