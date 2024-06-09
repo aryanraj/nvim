b0c325354d132bfc41ba3f864cefed881e3759bc
Aryan Raj <aryan.raj15604@gmail.com>
2024-06-09 21:28:37 +0530

    Enabled indent and incremental selection in treesitter

--- lua/core/plugin_config/treesitter.lua
+++ lua/core/plugin_config/treesitter.lua
@@ -8,4 +8,14 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
+  indent = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      node_incremental = "v",
+      node_decremental = "V",
+    },
+  },
 }
